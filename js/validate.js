0985e18332c71b521b4f4e3f9c9f649b6f1ec258
NataliaLKB <natalialeebaltazar@gmail.com>
2015-01-18 11:55:50 +0000

    attempting to form subit

--- js/validate.js
+++ js/validate.js
@@ -2,21 +2,26 @@
 *   VALIDATE.js
 *	Description: Email and form validation
 ************************************************/
+// (function() {
+//     'use strict'
 
-function validateForm() {
-    var name = document.forms["name"]["fname"].value;
-    if (x == null || x == "") {
-        alert("First name must be filled out");
-        return false;
-    }
-}
+    function validateForm() {
 
-function validateEmail() {
-    var value = document.forms["myForm"]["email"].value;
-    var at = value.indexOf("@");
-    var dot = value.lastIndexOf(".");
-    if (ats< 1 || dot<at+2 || dot+2>=x.length) {
-        alert("Not a valid e-mail address");
-        return false;
+        var name = document.forms["conversation"]["name"].value;
+
+        var email = document.forms["conversation"]["email"].value;
+        var at = email.indexOf("@");
+        var dot = email.lastIndexOf(".");
+
+        if (x == null || x == "") {
+            alert("First name must be filled out");
+            return false;
+        }
+
+        if (ats< 1 || dot<at+2 || dot+2>=x.length) {
+            alert("Not a valid e-mail address");
+            return false;
+        }
     }
-}
+
+// })();
