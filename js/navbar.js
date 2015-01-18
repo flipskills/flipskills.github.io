4ef6454746a821db01565607e3d5ed7d0a785afb
NataliaLKB <natalialeebaltazar@gmail.com>
2015-01-18 11:26:04 +0000

    added new navbar js function that works better

--- js/navbar.js
+++ js/navbar.js
@@ -1,31 +1,17 @@
-$(document).ready(function () {
-    var media = window.matchMedia("(max-width: 992)") 
+(function() {
 
-    var bsEnv = findBootstrapEnvironment();    
-    if(bsEnv === 'sm' || bsEnv === 'xs' ) {
-        $('.navbar').addClass('navbar-fixed-top');
-     }
-});
+    var $window = $(window);
 
-$(document).ready(function () {    
-    var bsEnv = findBootstrapEnvironment();    
-    if(bsEnv === 'md' || bsEnv === 'lg' ) {
-        $('.navbar').removeClass('navbar-fixed-top');
-     }
-});
-
-function findBootstrapEnvironment() {    
-    var envs = ['xs', 'sm', 'md', 'lg'];
-    $el = $('<div>');
-    $el.appendTo($('body'));
-
-    for (var i = envs.length - 1; i >= 0; i--) {
-        var env = envs[i];
-
-        $el.addClass('hidden-'+env);
-        if ($el.is(':hidden')) {
-            $el.remove();
-            return env
+    function resize() {
+        if ($window.width() < 992) {
+            return $('.navbar').addClass('navbar-fixed-top');
         }
-    };
-}
+
+        $('.navbar').removeClass('navbar-fixed-top');
+    }
+
+    $window
+        .resize(resize)
+        .trigger('resize');
+
+})();
