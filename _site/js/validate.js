3d4d3fbffd8b3440eaf4a12ef792272c441cc1c8
NataliaLKB <natalialeebaltazar@gmail.com>
2015-01-18 16:31:31 +0000

    subscribe message working

--- _site/js/validate.js
+++ _site/js/validate.js
@@ -29,8 +29,33 @@
             url: "http://getsimpleform.com/messages/ajax?form_api_token=4c8d34126c52221ab241246ba0712e3e",
             data: $(".ajax-form").serialize()
         }).done(function() {
+            $(".form-error").hide();
             $(".ajax-form").hide();
             $(".form-thank-you").fadeIn("400");
+        });
+            return false; //to stop the form from submitting
+        }
+    });
+
+    $(".subscribe-form").validate({
+        rules: {
+            emailSubscribe: {
+                required: true,
+                email: true
+            },
+        },
+        errorPlacement: function(error, element) {;
+            $(".subscribe-error").fadeIn("400");
+        },
+        submitHandler: function(form) {
+        $.ajax({
+            dataType: "jsonp",
+            url: "http://getsimpleform.com/messages/ajax?form_api_token=4c8d34126c52221ab241246ba0712e3e",
+            data: $(".ajax-form").serialize()
+        }).done(function() {
+            $(".subscribe-error").hide();
+            $(".subscribe-form").hide();
+            $(".subscribe-thank-you").fadeIn("400");
         });
             return false; //to stop the form from submitting
         }
