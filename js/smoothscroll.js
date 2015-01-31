59148ee4d93c2eb6b51edd520ecd6ad9eae8d50a
NataliaLKB <natalialeebaltazar@gmail.com>
2015-01-31 12:27:09 +0000

    edited smoothscroll so that scroll stops before the anchor

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -6,14 +6,24 @@
 $(document).ready(function() {
 	!function ($) {
         $('a[href^="#"]:not([data-toggle])').bind('click.smoothscroll',function (e) {
+            
             e.preventDefault();
             var target = this.hash;
                 $target = $(target);
-            $('html, body').stop().animate({
-                'scrollTop': $target.offset().top
-            }, 800, 'swing', function () {
-                window.location.hash = target;
-            });
+
+            if (window.innerWidth <= 768) {
+                $('html, body').stop().animate({
+                    'scrollTop': $target.offset().top-100
+                }, 800, 'swing', function () {
+                    window.location.hash = target;
+                });
+            } else {
+                $('html, body').stop().animate({
+                    'scrollTop': $target.offset().top-25
+                }, 800, 'swing', function () {
+                    window.location.hash = target;
+                });
+            }
         });
       }(window.jQuery)
 });
